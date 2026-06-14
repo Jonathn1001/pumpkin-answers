5ae088536d15ef37989d313b18821c4eda4b2d0c
elgnas <sangle1001.dev@gmail.com>
2026-06-14 15:27:55 +0700

    feat(configrepo): GORM Postgres repository + testcontainers contract

---
+++ engineer-challenge/internal/configrepo/migrate.go
@@ -0,0 +1,26 @@
+package configrepo
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+
+	"claimsplatform/migrations"
+)
+
+// Migrate applies all up migrations against the given Postgres URL.
+func Migrate(databaseURL string) error {
+	src, err := iofs.New(migrations.FS, ".")
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithSourceInstance("iofs", src, databaseURL)
+	if err != nil {
+		return err
+	}
+	defer m.Close()
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+	return nil
+}
